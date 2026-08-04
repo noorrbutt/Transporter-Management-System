ec58d62d02f692f7e2db7cebd103d4df27ce2d88
Noor Butt <nooramir.nb@gmail.com>
2026-08-04 22:51:40 +0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -195,6 +195,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
